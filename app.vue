bb1228e3da7fca2c0c72b161049ae091e8df75d3
小莫 <xiaomo1433675239@outlook.com>
2023-02-14 16:48:58 +0800

    添加百度统计代码

--- app.vue
+++ app.vue
@@ -3,6 +3,17 @@ import { useTheme } from "vuetify";
 const theme = useTheme();
 theme.global.name.value = "dark";
 
+useHead({
+    // 引入百度统计 https://hm.baidu.com/hm.js?0fba1f3013c5565107a4b39759647150
+    script: [
+        {
+            src: 'https://hm.baidu.com/hm.js?0fba1f3013c5565107a4b39759647150',
+            async: true,
+            defer: true,
+        }
+    ],
+})
+
 </script>
 <template>
     <NuxtLayout>
