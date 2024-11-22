c2576a0500c35187252216e33ba7fd014dbb6527
小莫 <xiaomo1433675239@outlook.com>
2024-11-22 15:54:16 +0800

    重置项目

--- .vitepress/config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitepress'
-
-// https://vitepress.dev/reference/site-config
-export default defineConfig({
-    title: "个人编程技术分享",
-    description: "小莫的个人网站, 用于记录并分享一些可能有用的东西",
-
-})
